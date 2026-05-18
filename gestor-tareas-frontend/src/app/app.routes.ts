44972fee2a8216d5f501e2ea55ac09278acb801d
Ivan Arasco <arascoivan97@gmail.com>
2026-05-18 13:52:48 +0200

    Add route tasklist component

--- gestor-tareas-frontend/src/app/app.routes.ts
+++ gestor-tareas-frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TasksList } from './components/tasks-list/tasks-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', component: TasksList },
+];
