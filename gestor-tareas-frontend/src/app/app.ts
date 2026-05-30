94b4c8d61ccc3db222f34f29a38bd6d9dd9a44ff
Ivan Arasco <arascoivan97@gmail.com>
2026-05-30 15:24:42 +0200

    Stylize

--- gestor-tareas-frontend/src/app/app.ts
+++ gestor-tareas-frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { NotificationComponent } from './components/notification/notification';
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, NotificationComponent],
+  imports: [RouterOutlet, Navbar, NotificationComponent, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
